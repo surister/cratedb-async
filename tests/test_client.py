efcf97219a43f3a41c7bf7775b8e2b206397854a
surister <surister98@gmail.com>
2025-04-04 16:45:41 +0200

    Fix test_bulk_query with invalid assert

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,17 +1,8 @@
-import trio
-
-from cratedb_async.client import CrateClient
-
-
-def test_client_request():
-    client = CrateClient('http://localhost:4200')
-    response = trio.run(client.query, 'create table (a text)')
-
+from cratedb_async.client import _create_insert_query
 
 def test_bulk_query():
-    client = CrateClient('')
+    query = _create_insert_query('t', row_count=1)
 
-    query = client._create_bulk_query('t', row_count=1) == 'insert into t values (?)'
     assert isinstance(query, str)
-    assert query == 'insert into t values (?)'
-    assert client._create_bulk_query('t', row_count=5) == 'insert into t values (?,?,?,?,?)'
+    assert query == 'insert into "t" values (?)'
+    assert _create_insert_query('t', row_count=5) == 'insert into "t" values (?,?,?,?,?)'
